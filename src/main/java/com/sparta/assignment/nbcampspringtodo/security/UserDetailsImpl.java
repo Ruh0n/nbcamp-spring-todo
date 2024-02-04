cd2182c6ed658aab90d1bd7f39dbba7fb80dd540
Ruh0n <wns8256@naver.com>
2024-02-04 11:07:10 +0900

    :lock: config security

--- src/main/java/com/sparta/assignment/nbcampspringtodo/security/UserDetailsImpl.java
+++ src/main/java/com/sparta/assignment/nbcampspringtodo/security/UserDetailsImpl.java
@@ -1,12 +1,28 @@
 package com.sparta.assignment.nbcampspringtodo.security;
 
 import com.sparta.assignment.nbcampspringtodo.user.User;
+import java.io.Serial;
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-public record UserDetailsImpl(User user) implements UserDetails {
+public final class UserDetailsImpl implements UserDetails {
+
+  @Serial
+  private static final long serialVersionUID = 0L;
+  private final User user;
+
+  private final Set<GrantedAuthority> authorities = new HashSet<>();
+
+
+  public UserDetailsImpl(User user) {
+    this.user = user;
+    authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+  }
 
   @Override
   public String getPassword() {
@@ -20,7 +36,7 @@ public record UserDetailsImpl(User user) implements UserDetails {
 
   @Override
   public Collection<? extends GrantedAuthority> getAuthorities() {
-    return new HashSet<>();
+    return authorities;
   }
 
   @Override
@@ -41,6 +57,28 @@ public record UserDetailsImpl(User user) implements UserDetails {
   @Override
   public boolean isEnabled() {
     return true;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (obj == this) {
+      return true;
+    }
+    if (obj == null || obj.getClass() != this.getClass()) {
+      return false;
+    }
+    var that = (UserDetailsImpl) obj;
+    return Objects.equals(this.user, that.user);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(user);
+  }
+
+  @Override
+  public String toString() {
+    return "UserDetailsImpl[" + "user=" + user + ']';
   }
 
 }
--- src/main/java/com/sparta/assignment/nbcampspringtodo/security/config/WebSecurityConfig.java
+++ src/main/java/com/sparta/assignment/nbcampspringtodo/security/config/WebSecurityConfig.java
@@ -9,7 +9,6 @@ import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -58,21 +57,19 @@ public class WebSecurityConfig {
     http.csrf(AbstractHttpConfigurer::disable);
 
     // 기본 설정인 Session 방식은 사용하지 않고 JWT 방식을 사용하기 위한 설정
-    http.sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+    http.sessionManagement(sessionManagement ->
+        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
-    http.authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests.requestMatchers(PathRequest.toStaticResources()
-                                                                                                  .atCommonLocations())
+    http.authorizeHttpRequests(registry -> registry.requestMatchers(PathRequest.toStaticResources()
+            .atCommonLocations())
         .permitAll() // resources 접근 허용 설정
-        .requestMatchers("/")
-        .permitAll() // 메인 페이지 요청 허가
-        .requestMatchers("/api/user/**")
-        .permitAll() // '/api/user/'로 시작하는 요청 모두 접근 허가
+        .requestMatchers("/api/user/signup", "/api/user/login")
+        .permitAll()
         .anyRequest()
-        .authenticated() // 그 외 모든 요청 인증처리
-    );
+        .authenticated());
 
 //    http.formLogin((formLogin) -> formLogin.loginPage("/api/user/login-page").permitAll());
-    http.formLogin(Customizer.withDefaults());
+//    http.formLogin(Customizer.withDefaults());
 
     // 필터 관리
     http.addFilterBefore(jwtAuthorizationFilter(), JwtAuthenticationFilter.class);
